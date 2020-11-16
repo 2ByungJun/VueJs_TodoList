86b51c4d583bd33a69182c5e44e7668a50778d1d
2ByungJun <ieed0205@naver.com>
2020-11-17 08:03:20 +0900

    #1 todoList commit

---
+++ src/components/CompletedTodo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span>완료 횟수 : {{ numberOfCompletedTodo }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        numberOfCompletedTodo(){
+            return this.$store.getters['todo/numberOfCompletedTodo'];
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
